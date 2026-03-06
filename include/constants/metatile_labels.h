87212543c79474bddfaa23caa7281cb7be114262
jonaswismar <jonaswismar@gmx.de>
2026-03-06 19:23:39 +0100

    Lilycove

--- include/constants/metatile_labels.h
+++ include/constants/metatile_labels.h
@@ -333,16 +333,15 @@
 #define METATILE_LavenderTown_Door  0x2A2
 
 // gTileset_Lilycove
-#define METATILE_Lilycove_Door             0x246
+#define METATILE_Lilycove_Door             0x2C6
 #define METATILE_Lilycove_Door_DeptStore   0x38C
 #define METATILE_Lilycove_Door_SafariZone  0x32D
-#define METATILE_Lilycove_Door_Wooden      0x28E
 #define METATILE_Lilycove_Wailmer0         0x290
-#define METATILE_Lilycove_Wailmer0_Alt     0x298
-#define METATILE_Lilycove_Wailmer1         0x291
-#define METATILE_Lilycove_Wailmer1_Alt     0x299
-#define METATILE_Lilycove_Wailmer2         0x2A0
-#define METATILE_Lilycove_Wailmer3         0x2A1
+#define METATILE_Lilycove_Wailmer0_Alt     0x318
+#define METATILE_Lilycove_Wailmer1         0x311
+#define METATILE_Lilycove_Wailmer1_Alt     0x319
+#define METATILE_Lilycove_Wailmer2         0x320
+#define METATILE_Lilycove_Wailmer3         0x321
 
 // gTileset_LilycoveMuseum
 #define METATILE_LilycoveMuseum_Painting0_Left   0x2DA
